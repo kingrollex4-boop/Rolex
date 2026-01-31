3b8e19f1b90052cc2a486cb5d18d7044b828ae25
Max <kingrollex4@gmail.com>
2026-01-31 04:08:28 -0500

    Create Settings.tsx
    Signed-off-by: Max <kingrollex4@gmail.com>

---
+++ src/src/src/src/contexts/src/components/Layout/src/components/Layout/src/components/Layout/src/components/src/components/src/components/src/components/Dashboard/src/components/Dashboard/src/components/Dashboard/src/components/Dashboard/src/providers/src/i18n/src/i18n/locales/en/(src/i18n/locales/es/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Settings: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-gray-800 dark:text-white">Settings</h1>
+      <p className="text-gray-600 dark:text-dark-300 mt-2">Configure application settings</p>
+    </div>
+  )
+}
+
+export default Settings
