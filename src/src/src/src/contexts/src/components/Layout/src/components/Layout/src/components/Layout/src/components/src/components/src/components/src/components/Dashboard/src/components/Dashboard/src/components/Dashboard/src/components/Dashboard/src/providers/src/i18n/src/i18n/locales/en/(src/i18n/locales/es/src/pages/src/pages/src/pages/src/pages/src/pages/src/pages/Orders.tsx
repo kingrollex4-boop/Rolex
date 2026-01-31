546fbc73bed9fe064029e52fce43a93d1c527a3c
Max <kingrollex4@gmail.com>
2026-01-31 04:07:19 -0500

    Create Orders.tsx
    Signed-off-by: Max <kingrollex4@gmail.com>

---
+++ src/src/src/src/contexts/src/components/Layout/src/components/Layout/src/components/Layout/src/components/src/components/src/components/src/components/Dashboard/src/components/Dashboard/src/components/Dashboard/src/components/Dashboard/src/providers/src/i18n/src/i18n/locales/en/(src/i18n/locales/es/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/Orders.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Orders: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-gray-800 dark:text-white">Orders</h1>
+      <p className="text-gray-600 dark:text-dark-300 mt-2">Manage customer orders and shipments</p>
+    </div>
+  )
+}
+
+export default Orders
